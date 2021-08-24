ed6b22ff55b3a62f76bcdae09d29c0dac2bd015c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-25 07:43:16 +0900

    スタイルを上書きする方法を学習。

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { string, bool } from 'prop-types';
+import { string, bool, shape } from 'prop-types';
 
-export const Hello = ({ children, bang }) => {
+export const Hello = ({ children, bang, style }) => {
   return (
     <View>
-      <Text style={styles.text}>
+      {/* 配列として書くことで同じプロパティであれば上書き、異なるプロパティなら足し合わせられる、後ろの方が優先される */}
+      <Text style={[styles.text, style]}>
         Hello {children}
         {bang ? '!' : ''}
       </Text>
@@ -16,10 +17,12 @@ export const Hello = ({ children, bang }) => {
 Hello.propTypes = {
   children: string.isRequired,
   bang: bool,
+  style: shape(),
 };
 
 Hello.defaultProps = {
   bang: false,
+  style: null,
 };
 
 const styles = StyleSheet.create({
