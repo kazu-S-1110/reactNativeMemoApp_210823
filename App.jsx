bfd698dc99520ee6bafa866b3fdbe2ff00498cf1
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-26 08:18:35 +0900

    circlebuttonのshadowの付け方に苦戦。なんとかできた！

--- App.jsx
+++ App.jsx
@@ -53,9 +53,14 @@ export default function App() {
 
       </View>
 
-      <View>
-        <Text>+</Text>
-      </View>
+      <CircleButton style={{
+        shadowOpacity: 0.75,
+        shadowRadius: 5,
+        shadowColor: 'black',
+        shadowOffset: { height: 2, width: 1 },
+      }}>
+        <CircleButtonLabel>+</CircleButtonLabel>
+      </CircleButton>
     </Container>
   )
 }
@@ -113,4 +118,21 @@ const MemoListItemDate = styled.Text`
   font-size: 15px;
   line-height: 16px;
   color: #848484;
+`
+const CircleButton = styled.View`
+  background-color: #467fd3;
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 19px;
+  bottom: 40px;
+
+`
+const CircleButtonLabel = styled.Text`
+  color: #ffffff;
+  font-size: 40px;
+  line-height: 40px;
 `
