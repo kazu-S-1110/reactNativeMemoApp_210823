1e0e852e7c23a9a266622840c787d8c4c00712d5
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-01 07:40:27 +0900

    ログイン画面をスタイリング。補完が効かなくて辛い。

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -6,16 +6,16 @@ export const LoginScreen = () => {
   return (
     <View style={styles.container}>
       <Appbar />
-      <View>
-        <Text>Log in</Text>
-        <TextInput value="Email Address" />
-        <TextInput value="Password" />
-        <View>
-          <Text>Submit</Text>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log in</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonLabel}>Submit</Text>
         </View>
-        <View>
-          <Text>Not registered?</Text>
-          <Text>Sign up here!</Text>
+        <View style={{ "flexDirection": "row" }}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
         </View>
       </View>
     </View>
@@ -24,6 +24,50 @@ export const LoginScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    backgroundColor: "#f0f4f8"
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: "700",
+    marginBottom: 24
+  },
+  input: {
+    fontSize: 17,
+    height: 48,
+    borderColor: "#DDD",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 8,
+    marginBottom: 16
+  },
+  buttonContainer: {
+    backgroundColor: "#467fd3",
+    borderRadius: 5,
+    alignSelf: "flex-start",
+    marginBottom: 24
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: "#fff",
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 10
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: "#467fd3"
   }
+
 })
