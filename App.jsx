32550f9d0c95d237eb6f507fdc1691cdf4691d81
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-01 12:35:33 +0900

    ReactNavigationを使ってみた。案外ややこしいかも、頑張るぞ〜

--- App.jsx
+++ App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import { LoginScreen } from './src/screens/LoginScreen'
 import { MemoCreateScreen } from './src/screens/MemoCreateScreen'
 import { MemoDetailScreen } from './src/screens/MemoDetailScreen'
@@ -7,14 +9,15 @@ import { MemoEditScreen } from './src/screens/MemoEditScreen'
 import { MemoListScreen } from './src/screens/MemoListScreen'
 import { SignUpScreen } from './src/screens/SignUpScreen'
 
+const Stack = createNativeStackNavigator()
+
 export default function App() {
   return (
-    <MemoListScreen />
-    // <MemoDetailScreen />
-    // <MemoEditScreen />
-    // <MemoCreateScreen />
-    // <LoginScreen />
-    // <SignUpScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
