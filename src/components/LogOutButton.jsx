8f871a142629fafd8734597e72bbbd229509d12f
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-04 23:34:06 +0900

    ログアウト機能を実装。案外簡単にできるものよね。

--- src/components/LogOutButton.jsx
+++ src/components/LogOutButton.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { TouchableOpacity, Text, StyleSheet, Alert } from 'react-native'
+import firebase from 'firebase'
+import { useNavigation } from '@react-navigation/native'
 
 export const LogOutButton = () => {
+  const navigation = useNavigation()
+  const handleLogout = () => {
+    firebase.auth().signOut()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "LogIn" }]
+        })
+      }).catch((err) => Alert.alert(err.message)
+      )
+  }
+
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={handleLogout}>
       <Text style={styles.label} >Log Out</Text>
     </TouchableOpacity>
   )
