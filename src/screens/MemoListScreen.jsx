900df0a497208b7962cda7c7532cc34abe2e5344
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-04 23:26:24 +0900

    ログアウトボタンを実装。navigationのsetOptionsを学習。

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { CircleButton } from '../components/CircleButton'
 import { Container } from '../components/Container'
+import { LogOutButton } from '../components/LogOutButton'
 import { MemoList } from '../components/MemoList'
 
 
 export const MemoListScreen = ({ navigation }) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    })
+  }, [])
   return (
     <Container>
       <MemoList />
